a0ccf64ff6375007b5fecec76db16c1a2503b1d1
Endrit <elimani0411@gmail.com>
2020-06-21 18:15:21 +0200

    Finish static pages

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,19 +1,27 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+    def setup
+        @base_title = "Ruby on Rails Sample App"
+    end
+    
+    
     test "should get home" do
         get root_url
         assert_response :success
+        assert_select "title", @base_title
     end
 
     test "should get help" do
         get page_url("help")
         assert_response :success
+        assert_select "title", "Help | #{@base_title}"
     end
 
     test "should get about" do
         get page_url("about")
         assert_response :success
+        assert_select "title", "About | #{@base_title}"
     end
 
 end
